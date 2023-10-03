16d814bb846986d37143ad13c3e76f2257d72b11
Avanishsri31 <90971881+Avanishsri31@users.noreply.github.com>
2023-10-04 00:19:33 +0530

    Update

--- src/components/SingleChat.jsx
+++ src/components/SingleChat.jsx
@@ -15,7 +15,7 @@ import io from "socket.io-client";
 import InputEmoji from "react-input-emoji";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "http://localhost:5000"; 
+const ENDPOINT = "https://vartalap-server.onrender.com"; 
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
